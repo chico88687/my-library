1293d565e97b08286daa0ac40bf57170fe28d97a
chico_vieira <fvieira9835@gmail.com>
2025-11-02 16:05:51 +0100

    Moved to the right directory

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import {Component, inject, OnInit} from '@angular/core';
 import {RouterOutlet} from '@angular/router';
 import {UserSettingsService} from './service/database/user-settings.service';
-import {NavBarComponent} from './shared-components/nav-bar/nav-bar.component';
+import {NavBarComponent} from './shared/nav-bar/nav-bar.component';
 
 @Component({
   selector: 'app-root',
