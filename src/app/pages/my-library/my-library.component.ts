e1583ec5be8c6d947acc3072753fd8e1a1272f75
chico_vieira <fvieira9835@gmail.com>
2025-11-03 13:52:53 +0100

    Advancements in the My-library component

--- src/app/pages/my-library/my-library.component.ts
+++ src/app/pages/my-library/my-library.component.ts
@@ -3,12 +3,14 @@ import {BookService} from '../../service/database/book.service';
 import {Book} from '../../database/tables/book';
 import {TableModule} from 'primeng/table';
 import {BookListComponent} from '../../shared/book-list/book-list.component';
+import {Button} from 'primeng/button';
+import {RouterLink} from '@angular/router';
 
 @Component({
   standalone: true,
   selector: 'my-library',
   templateUrl: './my-library.component.html',
-  imports: [TableModule, BookListComponent]
+  imports: [TableModule, BookListComponent, Button, RouterLink]
 })
 export class MyLibraryComponent implements OnInit {
 
