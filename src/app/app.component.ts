ae74586d4bfe05d4ff6611cc34aa855937ac9f17
chico_vieira <fvieira9835@gmail.com>
2025-11-08 18:50:28 +0100

    Capacitor status bar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import {RouterOutlet} from '@angular/router';
 import {UserSettingsService} from './service/database/user-settings.service';
 import {NavBarComponent} from './shared/nav-bar/nav-bar.component';
 import {AlertComponent} from './shared/alert/alert.component';
+import {StatusBar, Style} from '@capacitor/status-bar';
 
 @Component({
   selector: 'app-root',
@@ -15,6 +16,13 @@ export class AppComponent implements OnInit {
   protected readonly userSettingsService = inject(UserSettingsService);
 
   ngOnInit(): void {
+    StatusBar.setStyle({ style: Style.Dark })
+      .catch(err => console.warn('StatusBar style error', err));
+
+    StatusBar.setOverlaysWebView({ overlay: false })
+      .catch(err => console.warn('StatusBar overlay error', err));
+
     void this.userSettingsService.initialize();
   }
+
 }
