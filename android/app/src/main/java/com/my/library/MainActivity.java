3b3361279ac8bbba5f76bbd3c7f123f17d5ec327
chico_vieira <fvieira9835@gmail.com>
2025-11-08 17:01:03 +0100

    capacitor-set-up

---
+++ android/app/src/main/java/com/my/library/MainActivity.java
@@ -0,0 +1,5 @@
+package com.my.library;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.my.library',
+  appName: 'my-library-app',
+  webDir: 'dist/my-library-app/browser',
+};
+
+export default config;
