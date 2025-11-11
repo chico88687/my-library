628371885e96c6e147631484657c75f0dc12bf6c
chico_vieira <fvieira9835@gmail.com>
2025-11-11 22:06:43 +0100

    Page header in wish list component

--- src/app/pages/wish-list/wish-list.component.ts
+++ src/app/pages/wish-list/wish-list.component.ts
@@ -1,8 +1,12 @@
 import {Component} from '@angular/core';
+import {PageHeaderComponent} from '../../shared/page-header/page-header.component';
 
 @Component({
   standalone: true,
   selector: 'wish-list',
   templateUrl: './wish-list.component.html',
+  imports: [
+    PageHeaderComponent
+  ]
 })
 export class WishListComponent {}
