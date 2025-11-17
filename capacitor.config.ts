dacd53a27904f8fa0b07c6a5feb81726648932ac
chico_vieira <fvieira9835@gmail.com>
2025-11-17 19:45:16 +0100

    Trys

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,8 +2,20 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.my.library',
-  appName: 'my-library-app',
+  appName: 'My Library',
   webDir: 'dist/my-library-app/browser',
+
+  plugins: {
+    SplashScreen: {
+      launchShowDuration: 1000,
+      launchAutoHide: false,
+      androidScaleType: 'small',
+      showSpinner: false,
+      splashFullScreen: true,
+      splashImmersive: true,
+      backgroundColor: '#19191b',
+    }
+  }
 };
 
 export default config;
