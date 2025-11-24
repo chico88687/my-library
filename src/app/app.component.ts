c975ed948e0d42d4b316345f88c285d3cdc9b774
chico_vieira <fvieira9835@gmail.com>
2025-11-24 21:22:45 +0100

    Back button fixed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,8 @@ import {UserSettingsService} from './service/database/user-settings.service';
 import {NavBarComponent} from './shared/nav-bar/nav-bar.component';
 import {AlertComponent} from './shared/alert/alert.component';
 import {StatusBar, Style} from '@capacitor/status-bar';
+import {App} from '@capacitor/app';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-root',
@@ -14,6 +16,7 @@ import {StatusBar, Style} from '@capacitor/status-bar';
 export class AppComponent implements OnInit {
 
   protected readonly userSettingsService = inject(UserSettingsService);
+  private readonly location: Location = inject(Location);
 
   ngOnInit(): void {
     StatusBar.setStyle({ style: Style.Dark })
@@ -23,6 +26,19 @@ export class AppComponent implements OnInit {
       .catch(err => console.warn('StatusBar overlay error', err));
 
     void this.userSettingsService.initialize();
+    this.appBackNavigation();
+  }
+
+  appBackNavigation(): void {
+    App.addListener('backButton', ({canGoBack}) => {
+      if (canGoBack) {
+        // Go back in Angular routing history
+        this.location.back();
+      } else {
+        // Minimize or exit the app if user is on the root page
+        App.exitApp().then();
+      }
+    }).then();
   }
 
 }
